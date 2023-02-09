14e67f3d41c154efdef743830ad945b13eab5711
RoshanDPEA <114439704+RoshanDPEA@users.noreply.github.com>
2023-02-09 14:47:09 -0800

    Update newMain.py

--- newMain.py
+++ newMain.py
@@ -4,6 +4,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import _thread as thread
+import time
+
+def imageUpdate():
+    # https://stackoverflow.com/questions/40121382/control-chromium-kiosk-mode-url-from-python
+    chrome_options = Options()
+    chrome_options.add_argument("--kiosk")
+
+    # open driver
+    # https://chromedriver.chromium.org/getting-started
+    driver = webdriver.Chrome(executable_path='/tmp/chromedriver', chrome_options=chrome_options)
+    driver.set_window_position(2000, 0)
+
+    # go to website where images are
+    web_url = "https://en.wikipedia.org/wiki/Image"
+    driver.get(web_url)
+
+    # search for image by looking for keyword
+    im_link = driver.find_element_by_class_name("image")
+    im_link.click()
+    # open in new tab
+    # im_link.send_keys(Keys.CONTROL+Keys.RETURN)
+
+    # open new kiosk tab
+    # #https://www.tutorialspoint.com/how-to-close-active-current-tab-without-closing-the-browser-in-selenium-python
+
+    # choose where to open tab
+    # https://stackoverflow.com/questions/3816073/in-a-multi-monitor-display-environment-how-do-i-tell-selenium-which-display-to
+    # have a second thread control the second monitor
+
+    # identify element
+    m = driver.find_element_by_link_text("Help")
+    m.click()
+    # obtain parent window handle
+    p = driver.window_handles[0]
+    # obtain browser tab window
+    c = driver.window_handles[1]
+    # switch to tab browser
+    driver.switch_to.window(c)
+    print("Page title :")
+    print(driver.title)
+    # close browser tab window
+    driver.close()
+    # switch to parent window
+    driver.switch_to.window(p)
+    print("Current page title:")
+    print(driver.title)
+    # close browser parent window
+    driver.quit()
 
 #https://stackoverflow.com/questions/40121382/control-chromium-kiosk-mode-url-from-python
 chrome_options = Options()
@@ -24,7 +73,12 @@ im_link.click()
 #open in new tab
 #im_link.send_keys(Keys.CONTROL+Keys.RETURN)
 
-#open new kiosk tab https://www.tutorialspoint.com/how-to-close-active-current-tab-without-closing-the-browser-in-selenium-python
+#open new kiosk tab
+# #https://www.tutorialspoint.com/how-to-close-active-current-tab-without-closing-the-browser-in-selenium-python
+
+#choose where to open tab
+#https://stackoverflow.com/questions/3816073/in-a-multi-monitor-display-environment-how-do-i-tell-selenium-which-display-to
+#have a second thread control the second monitor
 
 #identify element
 m = driver.find_element_by_link_text("Help")
@@ -45,3 +99,6 @@ print("Current page title:")
 print(driver.title)
 #close browser parent window
 driver.quit()
+
+#second monitor
+thread.start_new_thread (imageUpdate())
